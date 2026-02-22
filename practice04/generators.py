2ec30098cb5771ad2787078978f4df32793ea412
MaratovaAizere <aizmaratova7@gmail.com>
2026-02-23 01:32:18 +0500

    Generators and iterators
    Refactor generator functions and update input prompts.

--- practice04/generators.py
+++ practice04/generators.py
@@ -1,4 +1,6 @@
-def square(n):#ex1
+#task1
+
+def square(n):
     for i in range(1, n+1):
         yield i * i
 n = int(input())
@@ -6,10 +8,14 @@ n = int(input())
 for i in square(n):
     print(i)
 
-n = int(input())#ex2
+#task2
+
+n = int(input())
 print(*(i for i in range(0, n + 1, 2)), sep=",")
 
-def devisible(n):#ex3
+#task3
+
+def devisible(n):
     for i in range(n + 1):
         if i % 3 == 0 and i%4 ==0:
             yield i
@@ -17,7 +23,9 @@ n=int(input())
 for i in devisible(n):
     print(i)
 
-def squares(a,b):#ex4
+#task4
+
+def squares(a,b):
     for i in range(a, b+1):
         yield i * i
 a=list(map(int,input().split()))
