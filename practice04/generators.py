459b1caf4eb45fca4ff71dc06ff28cd4d162aa4b
MaratovaAizere <aizmaratova7@gmail.com>
2026-02-23 01:33:42 +0500

    Generators and iterators

--- practice04/generators.py
+++ practice04/generators.py
@@ -1,4 +1,4 @@
-#task1
+#Task 1
 
 def square(n):
     for i in range(1, n+1):
@@ -8,12 +8,12 @@ n = int(input())
 for i in square(n):
     print(i)
 
-#task2
+#Task 2
 
 n = int(input())
 print(*(i for i in range(0, n + 1, 2)), sep=",")
 
-#task3
+#Task 3
 
 def devisible(n):
     for i in range(n + 1):
@@ -23,7 +23,7 @@ n=int(input())
 for i in devisible(n):
     print(i)
 
-#task4
+#Task 4
 
 def squares(a,b):
     for i in range(a, b+1):
@@ -32,5 +32,7 @@ a=list(map(int,input().split()))
 for i in squares(a[0],a[1]):
     print(i)
 
-n=int(input())#ex5
+#Task 5
+
+n=int(input())
 print(*(i for i in range(n, -1, -1)), sep=" ")
