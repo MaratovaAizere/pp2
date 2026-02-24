1e685a8af390b3d673f5b72a481794e4b8094a4d
MaratovaAizere <aizmaratova7@gmail.com>
2026-02-24 11:40:47 +0500

    Generators and Iterators
    Removed unused MyNumbers class and print statements.

--- practice04/generators.py
+++ practice04/generators.py
@@ -74,24 +74,3 @@ mystr = "banana"
 
 for x in mystr:
   print(x)
-
-#5
-
-class MyNumbers:
-  def __iter__(self):
-    self.a = 1
-    return self
-
-  def __next__(self):
-    x = self.a
-    self.a += 1
-    return x
-
-myclass = MyNumbers()
-myiter = iter(myclass)
-
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
