57770f9769fdd285a7054a6b725268841d1cea77
MaratovaAizere <aizmaratova7@gmail.com>
2026-03-31 08:25:51 +0500

    functions

--- practice08/functions.sql
+++ practice08/functions.sql
@@ -11,6 +11,46 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+
+-- 2. Upsert (insert или update)
+CREATE OR REPLACE PROCEDURE upsert_contact(p_name VARCHAR, p_phone VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM phonebook WHERE name = p_name) THEN
+        UPDATE phonebook
+        SET phone = p_phone
+        WHERE name = p_name;
+    ELSE
+        INSERT INTO phonebook(name, phone)
+        VALUES (p_name, p_phone);
+    END IF;
+END;
+$$;
+
+
+
+-- 3. Массовая вставка (bulk insert)
+CREATE OR REPLACE PROCEDURE bulk_insert_contacts(
+    p_names TEXT[],
+    p_phones TEXT[]
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    i INT;
+BEGIN
+    FOR i IN 1..array_length(p_names, 1) LOOP
+        INSERT INTO phonebook(name, phone)
+        VALUES (p_names[i], p_phones[i])
+        ON CONFLICT (name) DO UPDATE
+        SET phone = EXCLUDED.phone;
+    END LOOP;
+END;
+$$;
+
+
+
 -- 4. Пагинация
 CREATE OR REPLACE FUNCTION get_contacts_paginated(p_limit INT, p_offset INT)
 RETURNS TABLE(id INT, name VARCHAR, phone VARCHAR) AS $$
@@ -22,3 +62,15 @@ BEGIN
     LIMIT p_limit OFFSET p_offset;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+-- 5. Удаление
+CREATE OR REPLACE PROCEDURE delete_contact(p_value TEXT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM phonebook
+    WHERE name = p_value OR phone = p_value;
+END;
+$$;
